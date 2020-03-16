2aefead2fc855e07cf4aee5c8278ba3d258d101e
tawhidjony <tawhid.jony.jhm@gmail.com>
2020-03-16 23:42:45 +0600

    Updates

---
+++ precache-manifest.7fdb40e7cc3ddbc05c55851b224f327d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d377980a619dff21a929b9d6c549c098",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ffd266c92a733ba0b3cb",
+    "url": "/my-portfolio/static/css/main.79e0775a.chunk.css"
+  },
+  {
+    "revision": "46579047b1b4327ada43",
+    "url": "/my-portfolio/static/js/2.d23a1440.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/my-portfolio/static/js/2.d23a1440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd266c92a733ba0b3cb",
+    "url": "/my-portfolio/static/js/main.ff2ee790.chunk.js"
+  },
+  {
+    "revision": "7c7d8426a5976bd5dcaf",
+    "url": "/my-portfolio/static/js/runtime-main.ec1bad13.js"
+  },
+  {
+    "revision": "0d74c389d6bce681b72c4c93a232c4af",
+    "url": "/my-portfolio/static/media/tawhid2.0d74c389.png"
+  }
+]);
